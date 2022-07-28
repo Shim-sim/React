b7000304d405e1d0c53cc8ae23075dbdc37a5c94
Shimsim <timesea96@naver.com>
2022-07-28 11:02:21 +0000

    react-icons + page 이동 버튼 추가

--- voca/src/component/Day.js
+++ voca/src/component/Day.js
@@ -1,13 +1,16 @@
 /* eslint-disable */
 import Word from './Word.js'
-import { Link, useParams } from 'react-router-dom'
+import { Link, useParams, useNavigate } from 'react-router-dom'
 import useFetch from '../hooks/useFetch'
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io'
 
 const Day = () => {
 	
 	const { day } = useParams()
 	const words = useFetch(`https://study-json.run.goorm.io/words?day=${day}`)
-	
+	const navigate = useNavigate()
+	const page = Number(day)
+
 	return (
 	<>	
 		<h2>Day {day}</h2>
@@ -19,6 +22,10 @@ const Day = () => {
 				))}
 			</tbody>
 		</table>
+		<div className="paginate">
+		 <Link to={`/day/${page-1}`}><IoIosArrowBack /></Link>
+		 <Link to={`/day/${page+1}`}><IoIosArrowForward/></Link>
+		</div>
 	</>	
 	)
 }
